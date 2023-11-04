cc81a48decc5049d9c15e63a855f0ab5dbe05694
Shivam Mahajan <shivam.sm2002@gmail.com>
2023-11-05 00:26:11 +0530

    fix: navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,33 +1,17 @@
 import React, { useState, useEffect } from "react";
 import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Avatar } from "@mui/material";
 import { useUserContext } from "../store/Context";
 
 export default function Navbar({ isHomePage = true }) {
-  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
   const navigate = useNavigate();
   const { state } = useUserContext();
-  const { user, isLoggedIn, isSeller } = state;
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollPos = window.pageYOffset;
-      const isScrollingDown = currentScrollPos > prevScrollPos;
-
-      setPrevScrollPos(currentScrollPos);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [prevScrollPos]);
+  const { user, isLoggedIn } = state;
 
   return (
-    <div className={`fixed top-0 left-0 w-full z-10`}>
+    <div className={`absolute top-0 left-0 w-full z-10`}>
       <div className={`p-4 flex justify-between items-center`}>
         <div
           className={`flex items-center space-x-2 cursor-pointer ${
@@ -47,9 +31,12 @@ export default function Navbar({ isHomePage = true }) {
             isHomePage ? "text-white" : "text-black"
           }`}
         >
-          <h2 className="text-sm">English/INR</h2>
-          <HelpOutlineIcon className="text-xl" />
-          <h2 className="text-sm">Help</h2>
+          <Link to="/playground" className="hover:underline text-lg mx-2">
+            Playground
+          </Link>
+          <Link to="/upload" className="hover:underline text-lg mx-2">
+            Upload
+          </Link>
           {!isLoggedIn ? (
             <h2
               className={`text-sm border-2 px-2 py-1 rounded-lg cursor-pointer hover:bg-white hover:text-blue-500 ${
