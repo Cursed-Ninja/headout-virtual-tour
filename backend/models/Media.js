7fcbbb577961aa8d252ed4c31c6e06f2b6607a35
Shivam Mahajan <shivam.sm2002@gmail.com>
2023-11-04 18:49:52 +0530

    feat: add file storage in backend

---
+++ backend/models/Media.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const MediaSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        videos: [{ type: String }],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Media = mongoose.model("Media", MediaSchema);
+export default Media;
