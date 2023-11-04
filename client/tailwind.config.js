5b07bd21da50631465f71bec005d80400b8aed08
banjobyster <sayan25bak@gmail.com>
2023-11-04 18:02:11 +0530

    tailwind config setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/*.jsx"],
+  content: ["./src/**/*.jsx", "./index.html", "./src/**/*.css"],
   theme: {
     extend: {},
   },
